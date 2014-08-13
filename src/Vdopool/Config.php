c41a8d336eaf8cbd75967d8a5b84397191104b4f
gengzhgiuo <gengzhiguo@starschina.com>
2014-08-13 18:29:00 +0800

    删除配置信息

--- src/Vdopool/Config.php
+++ src/Vdopool/Config.php
@@ -7,18 +7,18 @@ namespace Vdopool;
 class Config
 {
   // 访问key
-  public static $access_key    = 'xaeL7591aE1uNvwV';
+  public static $access_key    = '';
 
   // 私有密钥
-  public static $secret_key = 'sp8sGL5qH0zGUlnpKrR8SNzvIy4ORqMbS7vMcPiZ';
+  public static $secret_key = '';
 
   // 商户设置的回调地址
   // public static $notify_url = 'http://payment.211.100.56.140.xip.io/test/notify';
 
   // 发送地址
-  public static $sendsms_url  = 'http://114.112.84.135:8081/api/v1/sendsms';
+  public static $sendsms_url  = '';
 
   // 验证地址
-  public static $verify_url  = 'http://114.112.84.135:8081/api/v1/verify';
+  public static $verify_url  = '';
 }
 
